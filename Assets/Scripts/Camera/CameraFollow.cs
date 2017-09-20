fc96006d5a3d63f9c19cc8e2fa47d58fe5158a4b
Clervens Volcy <clervens.volcy@gmail.com>
2017-09-20 19:18:49 -0400

    Finish the steps 3 and 4 of the tutorial

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothing = 5f;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		Vector3 targetCamPos = target.position + offset;
+
+		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Survival Shooter tutorial
+
+#### A isometric game that expand knowledge and experience of working with Unity.
+
+Survival Shooter is a isometric style shooter that helps to understand beginners level components of Unity.
+
+That project has been presented in Unite training day 2014.
+
+[Survival Shooter tutorial | unity3d.com](https://unity3d.com/learn/tutorials/projects/survival-shooter-tutorial)
